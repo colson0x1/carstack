26ff239c305411655dd93905398adcacca9089a7
Colson <colsoncalifornication@gmail.com>
2023-07-09 11:09:57 +0545

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import CarForm from './components/CarForm';
+import CarList from './components/CarList';
+import CarSearch from './components/CarSearch';
+import CarValue from './components/CarValue';
+
 function App() {
   return (
     <div>
-      <h1>CarStack</h1>
+      <CarForm />
+      <CarList />
+      <CarSearch />
+      <CarValue />
     </div>
   );
 }
