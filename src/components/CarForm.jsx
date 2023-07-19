bfea90921cadd887df66c7ee33f53200b9cae22c
Colson <colsoncalifornication@gmail.com>
2023-07-19 22:17:15 +0545

    dispatch car name, cost on form submission

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,13 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { changeName, changeCost } from '../store';
+import { changeName, changeCost, addCar } from '../store';
 
 function CarForm() {
   const dispatch = useDispatch();
-  const {name, cost } = useSelector((state) => {
+  const { name, cost } = useSelector((state) => {
     return {
       name: state.form.name,
-      cost: state.form.cost
-    }
+      cost: state.form.cost,
+    };
   });
 
   const handleNameChange = (event) => {
@@ -19,10 +19,16 @@ function CarForm() {
     dispatch(changeCost(carCost));
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch(addCar({ name, cost }));
+  };
+
   return (
     <div className="car-form panel">
       <h4 className="subtitle is-3">Add Car</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="field-group">
           <div className="field">
             <label className="label">Name</label>
@@ -44,6 +50,9 @@ function CarForm() {
               type="number"
             />
           </div>
+        </div>
+        <div className="field">
+          <button className="button is-link">Add</button>
         </div>
       </form>
     </div>
