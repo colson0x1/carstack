93c14ac0b9dc51f7714772d46abd3946e70d7c5a
Colson <colsoncalifornication@gmail.com>
2023-07-20 12:53:08 +0545

    compute total cost of filtered cars

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,5 +1,21 @@
+import { useSelector } from 'react-redux';
+
 function CarValue() {
-  return <div>CarValue</div>;
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+    if (!searchTerm) {
+      return data.reduce((accumulator, car) => {
+        return accumulator + car.cost;
+      }, 0);
+    }
+
+    return data
+      .filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((accumulator, car) => accumulator + car.cost, 0);
+  });
+
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
 }
 
 export default CarValue;
